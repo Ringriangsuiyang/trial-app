abd2833fbc55499be75c6659e3e568fdc058b4a7
Ringriangsuiyang <ringriangsuiyangpamei@yahoo.com>
2022-09-11 02:22:43 +0530

    storing in and fetching data from a database

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,34 +1,48 @@
-import MeetupList from '../components/meetups/MeetupList';
+import { useState, useEffect } from "react";
 
-
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'Squirrels Meetup',
-        image:
-          'https://static.vecteezy.com/system/resources/previews/004/338/987/non_2x/a-close-up-of-a-red-squirrel-standing-on-its-back-legs-against-an-old-tree-trunk-free-photo.jpg',
-        address: 'Meetupstree 5, 101 Meetup Forest',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
-      {
-        id: 'm2',
-        title: 'SpaceZ Meetup',
-        image:
-          'https://static.vecteezy.com/system/resources/previews/010/243/360/non_2x/the-big-moon-shines-behind-the-sea-with-stars-and-colorful-clouds-free-photo.jpg',
-        address: 'MeetupHeadquarter 6, 0069 Area 51',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
-]
+import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://react-try-ddd6c-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
     return (
-    <section>
-        <h1>All Meetups</h1>
-        <MeetupList meetups={DUMMY_DATA}/>
-    </section>
+      <section>
+        <p>Loading...</p>
+      </section>
     );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
